739e08502942c452365d022c9da2cce728f1157b
FernandaTurcios <jfturcios@correo.url.edu.gt>
2025-03-22 00:21:34 -0600

    Problema1

--- src/main/java/ed/lab/Main.java
+++ src/main/java/ed/lab/Main.java
@@ -3,6 +3,19 @@ package ed.lab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        TreeNode<Integer> root = new TreeNode<>(1);
+        root.left = new TreeNode<>(8);
+        root.right = new TreeNode<>(5);
+
+        System.out.println("Árbol:");
+        System.out.println(root);
+
+        // Crear instancia de E01InvertBT y ejecutar invertTree
+        E01InvertBT inverter = new E01InvertBT();
+        TreeNode<Integer> invertedRoot = inverter.invertTree(root);
+
+        System.out.println("Árbol invertido:");
+        System.out.println(invertedRoot);
     }
+
 }
